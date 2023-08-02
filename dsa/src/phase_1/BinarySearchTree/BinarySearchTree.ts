17ee5ac1e24e07113b21e02a88f0625fcdd433ed
Robert Montgomery <robm4958@gmail.com>
2023-08-01 20:06:14 -0400

    flake

--- dsa/src/phase_1/BinarySearchTree/BinarySearchTree.ts
+++ dsa/src/phase_1/BinarySearchTree/BinarySearchTree.ts
@@ -2,29 +2,56 @@ export class TreeNode {
   value: number;
   left: TreeNode | null;
   right: TreeNode | null;
+
+  constructor(value: number, left: TreeNode | null, right: TreeNode | null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 export class BinaryTree {
   root: TreeNode | null;
-  
+
   constructor(root: TreeNode) {
     this.root = root;
   }
-  
-  insert() {
-    return null;
+
+  insert(value: number) {
+    let prev: TreeNode | null = null;
+    let current: TreeNode | null = this.root;
+
+    while (current) {
+      prev = current;
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+
+    if (!prev) {
+      this.root = new TreeNode(value, null, null);
+    } else if (value < prev.value) {
+      prev.left = new TreeNode(value, null, null);
+    } else {
+      prev.right = new TreeNode(value, null, null);
+    }
   }
 
-  delete() {
-    return null;
+  /**
+  * transplants the target node into the current
+  */
+  delete_transplant(curr: TreeNode, prev: TreeNode | null, target: TreeNode | null) {
   }
 
-  get() {
-    return null;
+  delete(node: TreeNode) {
+  }
+
+  get(value: number) {
   }
 
   inorder() {
-    return null;
   }
 
   preorder() {
---
+++ problems/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A Flake for node / yarn";
+
+  outputs = { self, nixpkgs }: 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+      ]
+    }
+  };
+}
